198f1c7c3b5e22df17ae613815573348abd3d6a2
jukkapongtor <jukkapong.marsri@gmail.com>
2016-06-24 17:37:50 +0700

    edit plus and lower show cart : form post to get

--- module/index.php
+++ module/index.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	echo "<meta charset='utf-8'>";
 	include ('function.php');
 	connect_db();
 	switch ($_GET['data']) {
@@ -7,8 +8,7 @@
 		case "districts";districts();break;
 		case "zipcode";zipcode();break;
 		case "add_cart";add_cart();break;
-		case "amounttotal_cart";amounttotal_cart();break;
-		default: echo "<script>alert('เกิดข้อผิดพลาดในการใช้งาน ระบบจะนำคุณไปยังหน้าหลัก');window.location='index.php'</script>";break;
+		//default: echo "<script>alert('เกิดข้อผิดพลาดในการใช้งาน ระบบจะนำคุณไปยังหน้าหลัก');window.location='../index.php'</script>";break;
 	}
 
 ?>
